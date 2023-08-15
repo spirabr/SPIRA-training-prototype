23f832e0308b52dde66e2ab7624c065afa5ff8ec
danlawand <daniel.lawand@usp.br>
2023-08-15 13:40:02 +0200

    Rename train.py to model_pipeline.py

--- training-pipeline/model_pipeline.py
+++ training-pipeline/model_pipeline.py
@@ -2,17 +2,14 @@ from sklearn.metrics import mean_squared_error
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
-
-
-import matplotlib
-matplotlib.use('Agg')  # Use the Agg backend for non-interactive plotting
-
 import matplotlib.pyplot as plt
-
+import matplotlib
 from data_extraction import data_reading
 from data_split import training_and_test_set
 from feature_engineering import extract_features
-from hiperparameters_configuration import
+from hiperparameters_configuration import linear_regression
+
+matplotlib.use('Agg')  # Use the Agg backend for non-interactive plotting
 
 # Extração de dados
 housing = data_reading()
@@ -20,13 +17,12 @@ housing = data_reading()
 # Separação de conjunto de treino e de teste
 X_train, X_test, y_train, y_test = training_and_test_set(housing)
 
-
 # Feature engineering
 features_train = extract_features(X_train)
 features_test = extract_features(X_test)
 
 # Configuração de hiperparâmetros (linear regression tem zero)
-regr = LinearRegression()
+regr = linear_regression()
 
 # Treino
 regr.fit(features_train, y_train)
