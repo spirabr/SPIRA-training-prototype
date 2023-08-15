47bea76ef6712f5c2b9a054cf9350da68dbdf0c4
danlawand <daniel.lawand@usp.br>
2023-08-15 13:20:53 +0200

    Create a simple linear regression

--- training-pipeline/train.py
+++ training-pipeline/train.py
@@ -1,7 +1,46 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+
+
+import matplotlib
+matplotlib.use('Agg')  # Use the Agg backend for non-interactive plotting
+
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-index = pd.date_range("1/1/2000", periods=8)
-s = pd.Series(np.random.randn(5), index=["a", "b", "c", "d", "e"])
+# Extração de dados
+housing = pd.read_csv('training-pipeline/housing.csv')
 
-print(type(s))
+# Separação de conjunto de treino e de teste
+X_train, X_test, y_train, y_test = train_test_split(housing.median_income, housing.median_house_value, test_size = 0.2)
+
+
+# Feature engineering
+def extract_features(X):
+    return np.array(X).reshape(-1, 1)
+
+
+features_train = extract_features(X_train)
+features_test = extract_features(X_test)
+
+# Configuração de hiperparâmetros (linear regression tem zero)
+regr = LinearRegression()
+
+# Treino
+regr.fit(features_train, y_train)
+
+# Predição
+y_pred = regr.predict(features_test)
+
+# Métricas
+residuals = y_pred - y_test
+
+# Analytics
+plt.hist(residuals)
+plt.savefig('training-pipeline/plot.png')  # Save the plot as an image file
+
