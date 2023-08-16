2445bdadab4aa416a28e6b594007d7be0b33cadc
danlawand <daniel.lawand@usp.br>
2023-08-16 16:47:55 +0200

    Add data extraction and data split for a NN

--- training-pipeline/model_pipeline.py
+++ training-pipeline/model_pipeline.py
@@ -3,32 +3,41 @@ from data_split import training_and_test_set
 from feature_engineering import extract_features
 from hiperparameters_configuration import linear_regression
 from train import fit
-from prediction import predict
+from validation import predict
 from metrics import error_residual
 from analytics import plot
+import torch
 
 # Extração de dados
-housing = data_reading()
+dataset = data_reading()
+print("Length: ", len(dataset))
+example, label = dataset[0]
+print("Features: ", example.shape)
+print("Label: of index 0", label)
 
 # Separação de conjunto de treino e de teste
-X_train, X_test, y_train, y_test = training_and_test_set(housing)
+train_size = int(len(dataset)*0.8)
+test_size = len(dataset)-train_size
+train_dataset, test_dataset = training_and_test_set(dataset, train_size, test_size)
+print("{} examples for training and {} for testing".format(len(train_dataset), len(test_dataset)))
 
-# Feature engineering
-features_train = extract_features(X_train)
-features_test = extract_features(X_test)
 
-# Configuração de hiperparâmetros (linear regression tem zero)
-regr = linear_regression()
-
-# Treino
-regr = fit(regr, features_train, y_train)
-
-# Predição
-y_pred = predict(regr, features_test)
-
-# Métricas
-residuals = error_residual(y_pred, y_test)
-
-# Analytics
-plot(residuals)
+# # Feature engineering
+# features_train = extract_features(X_train)
+# features_test = extract_features(X_test)
+#
+# # Configuração de hiperparâmetros (linear regression tem zero)
+# regr = linear_regression()
+#
+# # Treino
+# regr = fit(regr, features_train, y_train)
+#
+# # Predição
+# y_pred = predict(regr, features_test)
+#
+# # Métricas
+# residuals = error_residual(y_pred, y_test)
+#
+# # Analytics
+# plot(residuals)
 
