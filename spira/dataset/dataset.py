8ff0739d99bd01aa71154d07762d45406fb1ce62
danlawand <daniel.lawand@usp.br>
2023-10-10 17:56:57 +0200

    Create methods of noise in Dataset class

--- spira/dataset/dataset.py
+++ spira/dataset/dataset.py
@@ -13,14 +13,18 @@ from torch.nn.utils.rnn import pad_sequence
 CONTROL_CLASS = 0
 PATIENT_CLASS = 1
 
+WAV = pd.DataFrame
+
+# TODO: make Wav a Class
+
 class Dataset(Dataset):
     """
     Class for load a train and test from dataset generate by import_librispeech.py and others
     """
 
-    def __init__(self, c, ap, datasets: list[pd.DataFrame], classes: pd.Series, noise):
-        assert (len(datasets) == len(classes), "Datasets and classes should have the same lengths")
-
+    def __init__(self, c, ap, wavs: list[WAV], classes: pd.Series, noise):
+        assert (len(wavs) == len(classes), "Datasets and classes should have the same lengths")
+        # TODO: Make a random class to deal with random stuff
         # set random seed
         random.seed(c['seed'])
         torch.manual_seed(c['seed'])
@@ -30,8 +34,8 @@ class Dataset(Dataset):
         self.c = c
         self.ap = ap
 
-        self.tuples_dataset_and_class = zip(datasets, classes)
-        self.datasets = datasets
+        self.wav_with_class_list = zip(wavs, classes)
+        self.wavs = wavs
         self.classes = classes
 
         self.noise = noise
@@ -40,67 +44,56 @@ class Dataset(Dataset):
         self.eval = False
         self.test = False
 
-        self.max_seq_len = max_seq_length_calculator(c, ap, self.c.dataset['padding_with_max_length'], self.train, self.datasets)
+        self.max_seq_len = self._calculate_max_seq_length()
 
-    def get_max_seq_length(self):
-        return self.max_seq_len
+    def _calculate_max_seq_length(self):
+        if self.c.dataset['max_seq_len']:
+            return self.c.dataset['max_seq_len']
+        if not self.c.dataset['padding_with_max_length'] or not self.train:
+            return None  # raise Exception("Properties unavailable")
+        min_len, max_len = _find_min_max_in_wav(self.c.audio['hop_length'], self.wavs)
 
-    def _get_feature_and_target(self, wav, class_name):
-        if self.c.dataset['split_wav_using_overlapping']:
-            start_slice = 0
-            features = []
-            targets = []
-            step = self.ap.sample_rate * self.c.dataset['step']
-            for end_slice in range(self.ap.sample_rate * self.c.dataset['window_len'], wav.shape[1], step):
-                spec = self.ap.get_feature_from_audio(wav[:, start_slice:end_slice]).transpose(1, 2)
-                features.append(spec)
-                targets.append(torch.FloatTensor([class_name]))
-                start_slice += step
-            if len(features) == 1:
-                feature = self.ap.get_feature_from_audio(
-                    wav[:, :self.ap.sample_rate * self.c.dataset['window_len']]).transpose(1, 2)
-                target = torch.FloatTensor([class_name])
-            elif len(features) < 1:
-                raise RuntimeError(
-                    "ERROR: Some sample in your dataset is less than {} seconds! Change the size of the overleapping window (CONFIG.dataset['window_len'])".format(
-                        self.c.dataset['window_len']))
-            else:
-                feature = torch.cat(features, dim=0)
-                target = torch.cat(targets, dim=0)
-        else:
-            # feature shape (Batch_size, n_features, timestamp)
-            feature = self.ap.get_feature_from_audio(wav)
-            # transpose for (Batch_size, timestamp, n_features)
-            feature = feature.transpose(1, 2)
-            # remove batch dim = (timestamp, n_features)
-            feature = feature.reshape(feature.shape[1:])
-            if self.c.dataset['padding_with_max_length']:
-                # padding for max sequence
-                zeros = torch.zeros(self.max_seq_len - feature.size(0), feature.size(1))
-                # append zeros before features
-                feature = torch.cat([feature, zeros], 0)
-                target = torch.FloatTensor([class_name])
-            else:
-                target = torch.zeros(feature.shape[0], 1) + class_name
-        return feature, target
+        print("The Max Time dim length is: {} (+- {} seconds)".format(max_len, (
+                max_len * self.c.audio['hop_length']) / self.ap.sample_rate))
+        print("The Min Time dim length is: {} (+- {} seconds)".format(min_len, (
+                min_len * self.c.audio['hop_length']) / self.ap.sample_rate))
+        return max_len
+
+    def __len__(self):
+        return len(self.wavs)
 
     def __getitem__(self, idx):
-        # TODO: Responder: As duas próximas linhas funcionam para objetos zip?
-        #  Supondo que funcione, é preferível manter a primeira opção?
-        wav = self.tuples_dataset_and_class[idx][0]
-        class_name = self.tuples_dataset_and_class[idx][1]
-
-        # wav = self.datasets[idx]
-        # class_name = self.classes[idx]
+        wav, class_name = self.wav_with_class_list[idx]
 
         # its assume that noise file is bigger than wav file !!
         if self.c.data_augmentation['insert_noise']:
-            self._insert_noise(wav, idx, class_name)
+            wav = self._insert_noise(wav, idx, class_name)
 
         return self._get_feature_and_target(wav, class_name)
 
-    def __len__(self):
-        return len(self.datasets)
+    # TODO: Decide if _insert_noise is a good function name
+    def _insert_noise(self, wav: WAV, idx, class_name) -> WAV:
+        # Experiments do different things within the dataloader. So depending on the experiment we will have a different random state here in get item.
+        # To reproduce the tests always using the same noise we need to set the seed again, ensuring that all experiments see the same noise in the test !!
+        if self.test:
+            self._apply_random_seed(self.c['seed'] * idx) # multiply by idx, because if not we generate same some for all files !
+
+        return self._add_noise_wav_to_input_wav(wav, class_name)
+
+    # TODO: Decide if apply_random_seed is a good function name
+    def _apply_random_seed(self, seed_value: int):
+        random.seed(seed_value)
+        torch.manual_seed(seed_value)
+        torch.cuda.manual_seed(seed_value)
+        np.random.seed(seed_value)
+
+    # TODO: Decide if _add_noise_wav_to_wav is a good function name
+    def _add_noise_wav_to_input_wav(self, wav: WAV, class_name) -> WAV:
+        for _ in range(self._define_num_noise(class_name)):
+            noise_wav = self._calculate_noise_wav(wav)
+            wav = wav + noise_wav
+
+        return wav
 
     def _define_num_noise(self, class_name: int) -> int:
         if class_name == CONTROL_CLASS:
@@ -108,43 +101,82 @@ class Dataset(Dataset):
         if class_name == PATIENT_CLASS:
             return self.c.data_augmentation['num_noise_patient']
 
-    def _insert_noise(self, wav, idx, class_name):
-        # Experiments do different things within the dataloader. So depending on the experiment we will have a different random state here in get item. To reproduce the tests always using the same noise we need to set the seed again, ensuring that all experiments see the same noise in the test !!
-        if self.test:
-            random.seed(
-                self.c['seed'] * idx)  # multiply by idx, because if not we generate same some for all files !
-            torch.manual_seed(self.c['seed'] * idx)
-            torch.cuda.manual_seed(self.c['seed'] * idx)
-            np.random.seed(self.c['seed'] * idx)
+    # TODO: Decide if _calculate_noise_wav is a good function name
 
-        for _ in range(self._define_num_noise(class_name)):
-            # choose random noise file
-            noise_wav = self.noise[random.randint(0, len(self.noise))][0]
-            noise_wav_len = noise_wav.shape[1]
-            wav_len = wav.shape[1]
-            noise_start_slice = random.randint(0, noise_wav_len - (wav_len + 1))
-            # sum two different noise
-            noise_wav = noise_wav[:, noise_start_slice:noise_start_slice + wav_len]
-            # get random max amp for noise
-            max_amp = random.uniform(self.c.data_augmentation['noise_min_amp'],
-                                     self.c.data_augmentation['noise_max_amp'])
-            reduce_factor = max_amp / float(noise_wav.max().numpy())
-            noise_wav = noise_wav * reduce_factor
-            wav = wav + noise_wav
+    def _calculate_noise_wav(self, wav: WAV) -> WAV:
+        # choose random noise file
+        noise_wav = self.noise[random.randint(0, len(self.noise))][0]
+        noise_wav_len = noise_wav.shape[1]
+        wav_len = wav.shape[1]
+        noise_start_slice = random.randint(0, noise_wav_len - (wav_len + 1))
+        # sum two different noise
+        noise_wav = noise_wav[:, noise_start_slice:noise_start_slice + wav_len]
+        # get random max amp for noise
+        max_amp = random.uniform(self.c.data_augmentation['noise_min_amp'],
+                                 self.c.data_augmentation['noise_max_amp'])
+        reduce_factor = max_amp / float(noise_wav.max().numpy())
+        return noise_wav * reduce_factor
 
+    def _get_feature_and_target(self, wav: WAV, class_name):
+        if self.c.dataset['split_wav_using_overlapping']:
+            return self._get_feature_and_target_using_overlapping(wav, class_name)
 
-def max_seq_length_calculator(c, ap, padding_with_max_length, train_mode, datasets: list[pd.DataFrame]):
-    if c.dataset['max_seq_len']:
-        return c.dataset['max_seq_len']
-    if not padding_with_max_length or not train_mode:
-        return None # raise Exception("Properties unavailable")
-    min_len, max_len = _find_min_max_in_wav(c.audio['hop_length'], datasets)
+        # TODO: Should I do a function for the next three commands?
 
-    print("The Max Time dim length is: {} (+- {} seconds)".format(max_len, (
-            max_len * c.audio['hop_length']) / ap.sample_rate))
-    print("The Min Time dim length is: {} (+- {} seconds)".format(min_len, (
-            min_len * c.audio['hop_length']) / ap.sample_rate))
-    return max_len
+        # feature shape (Batch_size, n_features, timestamp)
+        feature = self.ap.get_feature_from_audio(wav)
+        # transpose for (Batch_size, timestamp, n_features)
+        feature = feature.transpose(1, 2)
+        # remove batch dim = (timestamp, n_features)
+        feature = feature.reshape(feature.shape[1:])
+
+        # TODO: Should I do functions for the if-else clauses?
+
+        if self.c.dataset['padding_with_max_length']:
+            # padding for max sequence
+            zeros = torch.zeros(self.max_seq_len - feature.size(0), feature.size(1))
+            # append zeros before features
+            feature = torch.cat([feature, zeros], 0)
+            target = torch.FloatTensor([class_name])
+        else:
+            target = torch.zeros(feature.shape[0], 1) + class_name
+
+        return feature, target
+
+    def _get_feature_and_target_using_overlapping(self, wav, class_name):
+        features, targets = self._append_features_and_targets_using_window_len(wav, class_name)
+
+        if len(features) == 1:
+            feature = self.ap.get_feature_from_audio(
+                wav[:, :self.ap.sample_rate * self.c.dataset['window_len']]).transpose(1, 2)
+            target = torch.FloatTensor([class_name])
+            return feature, target
+
+        # TODO: How should I do that error check before? Or Is it even possible to get that error at this time?
+        if len(features) < 1:
+            raise RuntimeError(
+                "ERROR: Some sample in your dataset is less than {} seconds! Change the size of the overleapping window (CONFIG.dataset['window_len'])".format(
+                    self.c.dataset['window_len']))
+
+        feature = torch.cat(features, dim=0)
+        target = torch.cat(targets, dim=0)
+
+        return feature, target
+
+    def _append_features_and_targets_using_window_len(self, wav, class_name):
+        start_slice = 0
+        features = []
+        targets = []
+        step = self.ap.sample_rate * self.c.dataset['step']
+        for end_slice in range(self.ap.sample_rate * self.c.dataset['window_len'], wav.shape[1], step):
+            spec = self.ap.get_feature_from_audio(wav[:, start_slice:end_slice]).transpose(1, 2)
+            features.append(spec)
+            targets.append(torch.FloatTensor([class_name]))
+            start_slice += step
+        return features, targets
+
+    def get_max_seq_length(self):
+        return self.max_seq_len
 
 
 def _find_min_max_in_wav(hop_length, datasets: list[pd.DataFrame]):
