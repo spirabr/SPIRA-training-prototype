93c0ca3e9d772136a147be008d0938d139104dd7
danlawand <daniel.lawand@usp.br>
2024-01-08 11:20:11 +0100

    remove commented code

--- spira/core/domain/dataloader.py
+++ spira/core/domain/dataloader.py
@@ -4,13 +4,12 @@ from spira.core.domain.dataset import Dataset
 
 
 def create_train_dataloader(
-        train_dataset: Dataset, batch_size: int, num_workers: int
+    train_dataset: Dataset, batch_size: int, num_workers: int
 ) -> DataLoader:
     return DataLoader(
         dataset=train_dataset,
         batch_size=batch_size,
         num_workers=num_workers,
-        # collate_fn=_basic_collate_fn,
         shuffle=True,
         pin_memory=True,
         drop_last=True,
@@ -19,18 +18,11 @@ def create_train_dataloader(
 
 
 def create_test_dataloader(
-        test_dataset: Dataset, batch_size: int, num_workers: int
+    test_dataset: Dataset, batch_size: int, num_workers: int
 ) -> DataLoader:
     return DataLoader(
         dataset=test_dataset,
         batch_size=batch_size,
         num_workers=num_workers,
-        # collate_fn=_basic_collate_fn,
         shuffle=False,
     )
-
-# def _basic_collate_fn(
-#         batch: list[tuple[Audio, int]]
-# ) -> tuple[list[Wav], list[Label]]:
-#     audios, labels = cast(tuple[list[Audio], list[int]], zip(*batch))
-#     return get_wavs_from_audios(audios), labels
